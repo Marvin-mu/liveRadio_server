48610066bdaf2617b3458842e2089a78dac5a849
marvin <muchaowen@yeah.net>
2020-06-12 22:33:29 +0800

    commit

---
+++ manageripport.cpp
@@ -0,0 +1,6 @@
+#include "manageripport.h"
+
+ManagerIpPort::ManagerIpPort()
+{
+
+}
---
+++ manageripport.h
@@ -0,0 +1,11 @@
+#ifndef MANAGERIPPORT_H
+#define MANAGERIPPORT_H
+
+
+class ManagerIpPort
+{
+public:
+    ManagerIpPort();
+};
+
+#endif // MANAGERIPPORT_H
